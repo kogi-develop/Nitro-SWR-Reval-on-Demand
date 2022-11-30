597fc0f9fed6a42b7e7cfa584118951fca792e8d
GregorKoglbauer <93713466+GregorKoglbauer@users.noreply.github.com>
2022-11-30 22:35:23 +0100

    1 file modified

--- routes/revalidate.ts
+++ routes/revalidate.ts
@@ -2,7 +2,6 @@ import { revalidateSwrCache } from '../utils/swrRevalidate';
 
 export default defineEventHandler(async (event) => {
   console.log('reval');
-  const router = await useStorage();
   const reval = await revalidateSwrCache({ name: 'index', route: '/' });
 
   return { reval };
